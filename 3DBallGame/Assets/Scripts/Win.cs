50d874eeee48da63407e2a4b514f992c8bfec61b
rambo5x <michaelm12x@gmail.com>
2021-01-09 05:48:35 -0600

    Official Source Code.
    Publication of Source Code.

---
+++ 3DBallGame/Assets/Scripts/Win.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Win : MonoBehaviour
+{
+    public Text wintxt;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        wintxt.gameObject.SetActive(true);
+        Destroy(player);
+    }
+
+}
